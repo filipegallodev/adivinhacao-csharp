86182f319c340ac6fb311c9fd16589836ad5c0bf
Filipe <fgallo_dev@hotmail.com>
2022-09-08 09:23:43 -0300

    Projeto criado

---
+++ JogoAdivinhacao/Numeros.cs
@@ -0,0 +1,37 @@
+﻿namespace JogoAdivinhacao
+{
+    public class Numeros
+    {
+        public Numeros(int numeroDistancia)
+        {
+           NumeroDistancia = numeroDistancia;
+        }
+
+        public int NumeroAleatorio { get; set; }
+        public int NumeroEscolhido { get; set; }
+        public int NumeroDistancia { get; set; }
+
+        public void GerarNumero()
+        {
+            Random rnd = new();
+            NumeroAleatorio = rnd.Next(10);
+        }
+
+        public void EscolherNumero()
+        {
+            int numeroEscolhido = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void VerificarNumero()
+        {
+            if (NumeroEscolhido == NumeroAleatorio)
+            {
+                Console.WriteLine($"Parabéns!! Você acertou em cheio!");
+            }
+            else
+            {
+                Console.WriteLine($"Não foi dessa vez... o número aleatório era {NumeroAleatorio}!");
+            }
+        }
+    }
+}
---
+++ JogoAdivinhacao/Program.cs
@@ -0,0 +1,16 @@
+﻿using JogoAdivinhacao;
+
+Console.WriteLine("Bem-vindo ao jogo da Adivinhação!");
+
+while (true) 
+{
+    Console.WriteLine("\nTente adivinhar o número aleatório entre 0 à 10:");
+
+    Numeros num = new Numeros(10);
+    num.GerarNumero();
+
+    num.EscolherNumero();
+
+    num.VerificarNumero();
+}
+
